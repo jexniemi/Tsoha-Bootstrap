f1292cd165f41275e263439eb9eea6248329d9cb
JN <jxnieminen@gmail.com>
2017-03-22 19:45:32 +0200

    alustavat etusivut, listaussivut, muokkaus- ja esittelysivut tehty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,39 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  echo 'Tamä on etusivu!';
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tamä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      View::make('helloworld.html');
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function front() {
+        View::make('suunnitelmat/frontpage.html');
+    }
+
+    public static function browseProfiles() {
+        View::make('suunnitelmat/browseFriends.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+    public static function profile() {
+        View::make('suunnitelmat/myProfile.html');
+    }
+
+    public static function editProfile() {
+        View::make('suunnitelmat/myProfileEdit.html');
+    }
+
+    public static function messages() {
+        View::make('suunnitelmat/messages.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,33 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/test', function() {
+    HelloWorldController::front();
+});
+
+$routes->get('/test/browse', function() {
+    HelloWorldController::browseProfiles();
+});
+
+$routes->get('/test/login', function() {
+    HelloWorldController::login();
+});
+
+$routes->get('/test/myprofile', function() {
+    HelloWorldController::profile();
+});
+
+$routes->get('/test/editprofile', function() {
+    HelloWorldController::editProfile();
+});
+
+$routes->get('/test/messages', function() {
+    HelloWorldController::messages();
+});
